b5225968e3f6d5cefaadbbb5769c933d91ac6b3e
Sailesh Bechar <sbechar@uwaterloo.ca>
2023-07-21 14:24:29 -0400

    slightly changed context collapse colours

--- src/components/Chat/ContextCollapse.tsx
+++ src/components/Chat/ContextCollapse.tsx
@@ -21,13 +21,13 @@ const ChatContextCollapse: Component<{ context: Context[] }> = (props) => {
   return (
     <div
       tabindex="0"
-      class="collapse collapse-arrow bg-secondary-focus text-secondary-content"
+      class="collapse collapse-arrow bg-secondary-content text-secondary-focus"
     >
       <div class="collapse-title font-medium h-[45px]">Sources</div>
       <div class="collapse-content">
         <For each={props.context}>
           {(item) => (
-            <div class="p-2 sm:p-10 rounded-lg my-4 bg-secondary">
+            <div class="p-2 sm:p-10 rounded-lg my-4 bg-secondary text-secondary-content">
               <div>{item.paragraph_text}</div>
               <div class="inline-flex justify-between gap-4 mt-4 w-full">
                 <div>{capitalizeTitle(item.chapter_name)}</div>
